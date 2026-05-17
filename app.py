cce0b0a8847a8cdbbe1f358b9ce382c9041a38ca
ranjith <ranjithg155@gmail.com>
2026-05-17 20:42:25 +0530

    Initial commit

--- app.py
+++ app.py
@@ -229,5 +229,4 @@ def page_not_found(e):
 
 
 if __name__ == "__main__":
-    debug = os.environ.get("FLASK_DEBUG", "0") == "1"
-    app.run(debug=debug, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+   app.run(debug=True)
